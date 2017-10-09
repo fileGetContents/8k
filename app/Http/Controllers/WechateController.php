6cf3a4bd7b53b9adb9463ce59b553854a244b254
yuzhichao <1932425337@qq.com>
2017-10-09 15:10:16 +0800

    wx2

--- app/Http/Controllers/WechateController.php
+++ app/Http/Controllers/WechateController.php
@@ -8,15 +8,33 @@ use App\Http\Wechate;
 class WechateController extends WebController
 {
 
-    public function userInfo()
+    public function userInfo(Request $request)
     {
         $config = new Wechate\WxPayConfig();
         if (isset($_GET['code'])) {
             // https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
             $accJson = file_get_contents('https://api.weixin.qq.com/sns/oauth2/access_token?appid=' . $config::APPID . '&secret=' . $config::APPSECRET . '&code=' . $_GET["code"] . '&grant_type=authorization_code ');
             $accArray = json_decode($accJson, true);
-            $info = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token=' . $accArray["access_token"] . '&openid=' . $accArray['openid'] . '&lang=zh_CN ');
-            var_dump($info);
+            $infoJson = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token=' . $accArray["access_token"] . '&openid=' . $accArray['openid'] . '&lang=zh_CN ');
+            $infoArray = json_encode($infoJson, true);
+            // 验证是否注册
+            $whether = $this->PurposeModel->selectFirst('use', [
+                'nick' => $infoArray['nickname'],
+                'openid' => $infoArray['openid'],
+                'headimgurl' => $infoArray['headimgurl'],
+            ]);
+            if (!is_null($whether)) {
+                $request->session()->put('user_id', $whether->id);
+                // 更新头像
+                $this->PurposeModel->up('use', ['id' => $whether->id], ['headimgurl' => $infoArray['headimgurl']]);
+            } else {
+                $id = $this->PurposeModel->insertGetId('use', [
+                    'nick' => $infoArray['nickname'],
+                    'openid' => $infoArray['openid'],
+                    'headimgurl' => $infoArray['headimgurl'],
+                ]);
+                $request->session()->put('user_id', $id);
+            }
         } else {
             $baseUrl = urlencode('http://www.xcylkj.com/public/wx/user/info');
             $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $config::APPID . '&redirect_uri=' . $baseUrl . '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
