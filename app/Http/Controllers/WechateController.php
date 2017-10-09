ceaa6d320403ec802c50ce573e9f7196b467583e
yuzhichao <1932425337@qq.com>
2017-10-09 11:59:22 +0800

    wx2

--- app/Http/Controllers/WechateController.php
+++ app/Http/Controllers/WechateController.php
@@ -17,9 +17,8 @@ class WechateController extends WebController
             // https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
             echo $_GET['code'];
         } else {
-
             $baseUrl = urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']);
-            $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $config::APPID . '&redirect_uri=' . $baseUrl . '&response_type=code&scope=SCOPE&state=STATE#wechat_redirect ';
+            $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $config::APPID . '&redirect_uri=' . $baseUrl . 'Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
             echo '<script type="text/javascript">window.location.href="' . $url . '";</script>';
         }
 
