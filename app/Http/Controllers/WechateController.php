d24eb22ad8f6d2744f773faa40b7ffbeebb098ae
yuzhichao <1932425337@qq.com>
2017-10-13 15:00:46 +0800

    'wxlogin'

--- app/Http/Controllers/WechateController.php
+++ app/Http/Controllers/WechateController.php
@@ -38,9 +38,9 @@ class WechateController extends WebController
                 ]);
                 session(['user_id' => $id]);
             }
-            echo '<script type="text/javascript">window.location.href="http://www.xcylkj.com/person"</script>';
+            echo '<script type="text/javascript">window.location.href="http://www.xcylkj.com/public/person"</script>';
         } else {
-            $baseUrl = urlencode('http://www.xcylkj.co/wx/user/info');
+            $baseUrl = urlencode('http://www.xcylkj.com/public/wx/user/info');
             $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $config::APPID . '&redirect_uri=' . $baseUrl . '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
             echo '<script type="text/javascript">window.location.href="' . $url . '";</script>';
         }
