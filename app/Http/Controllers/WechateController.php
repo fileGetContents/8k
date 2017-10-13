815888aac9bb98e6c4add20ace74c8f9fe033f3b
yuzhichao <1932425337@qq.com>
2017-10-13 15:12:43 +0800

    'wxlogin'

--- app/Http/Controllers/WechateController.php
+++ app/Http/Controllers/WechateController.php
@@ -22,12 +22,12 @@ class WechateController extends WebController
             $infoJson = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token=' . $accArray["access_token"] . '&openid=' . $accArray['openid'] . '&lang=zh_CN ');
             $infoArray = json_decode($infoJson, true);
             $whether = $this->PurposeModel->selectFirst('use', [
-                'nick' => $infoArray['nickname'],
+                // 'nick' => $infoArray['nickname'],
                 'openid' => $infoArray['openid'],
-                'headimgurl' => $infoArray['headimgurl'],
+                // 'headimgurl' => $infoArray['headimgurl'],
             ]);
             if (!is_null($whether)) {
-                $request->session()->put('user_id', $whether->id);
+                session(['user_id' => $whether->id]);
                 // 更新头像
                 $this->PurposeModel->up('use', ['id' => $whether->id], ['headimgurl' => $infoArray['headimgurl']]);
             } else {
