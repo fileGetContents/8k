8f3508bf7b14efd019286d14d85065788f46aa39
yuzhichao <1932425337@qq.com>
2017-10-15 18:39:13 +0800

    wxlogin

--- app/Http/Controllers/WechateController.php
+++ app/Http/Controllers/WechateController.php
@@ -77,16 +77,12 @@ class WechateController extends WebController
      */
     public function userTagsCreate(Request $request)
     {
-
-        die;
-        $opendId = $this->wxUserLogin('http://www.xcylkj.com/user/tag/' . $request->id);
-        dump($opendId);
-        die;
-        if (is_numeric($request->id)) { // 添加标签
-            $this->createUserTag($opendId);
-        } else {  // 删除标签
-            $this->delUserTag($opendId);
-        }
+//        $opendId = $this->wxUserLogin('http://www.xcylkj.com/user/tag/' . $request->id);
+//        if (is_numeric($request->id)) { // 添加标签
+//            $this->createUserTag($opendId);
+//        } else {  // 删除标签
+//            $this->delUserTag($opendId);
+//        }
         return view('changcreate');
     }
 
