99e4e7553d762cda04d6874d0f2a0330c5fb0e5f
yuzhichao <1932425337@qq.com>
2017-10-15 19:45:54 +0800

    wxlogin

--- resources/views/Web/identifyxianxing.blade.php
+++ resources/views/Web/identifyxianxing.blade.php
@@ -84,30 +84,30 @@
     });
 </script>
 
-<script type="text/javascript">
-    //调用微信JS api 支付
-    function jsApiCall() {
-        WeixinJSBridge.invoke(
-                'getBrandWCPayRequest',
-                <?php echo $jsApiParameters ?>,
-                function (res) {
-                    WeixinJSBridge.log(res.err_msg);
-                }
-        );
-    }
-    function callpay() {
-        if (typeof WeixinJSBridge == "undefined") {
-            if (document.addEventListener) {
-                document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
-            } else if (document.attachEvent) {
-                document.attachEvent('WeixinJSBridgeReady', jsApiCall);
-                document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
-            }
-        } else {
-            jsApiCall();
-        }
-    }
-</script>
+{{--<script type="text/javascript">--}}
+    {{--//调用微信JS api 支付--}}
+    {{--function jsApiCall() {--}}
+        {{--WeixinJSBridge.invoke(--}}
+                {{--'getBrandWCPayRequest',--}}
+                {{--<?php echo $jsApiParameters ?>,--}}
+                {{--function (res) {--}}
+                    {{--WeixinJSBridge.log(res.err_msg);--}}
+                {{--}--}}
+        {{--);--}}
+    {{--}--}}
+    {{--function callpay() {--}}
+        {{--if (typeof WeixinJSBridge == "undefined") {--}}
+            {{--if (document.addEventListener) {--}}
+                {{--document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);--}}
+            {{--} else if (document.attachEvent) {--}}
+                {{--document.attachEvent('WeixinJSBridgeReady', jsApiCall);--}}
+                {{--document.attachEvent('onWeixinJSBridgeReady', jsApiCall);--}}
+            {{--}--}}
+        {{--} else {--}}
+            {{--jsApiCall();--}}
+        {{--}--}}
+    {{--}--}}
+{{--</script>--}}
 
 </body>
 </html>
