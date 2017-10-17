a215e506f7e4f210d8aad2e5741692bf3e928dba
yuzhichao <1932425337@qq.com>
2017-10-17 18:58:36 +0800

    wx

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -39,6 +39,7 @@ class ColumnController extends WebController
         // 查看是否添加手机号
         $userInfo = $this->PurposeModel->selectFirst('use', ['id' => session('user_id', 1)]);
         if ($userInfo->telephone == null) {
+            echo '<meta charset="utf-8"/>';
             echo '<script>alert("请先添加手机号")</script>';
             echo '<script>window.location.href= ' . URL('person') . '</script>';
             die;
