dbf8db858b6f1fbfd26b252137ad4f0ca98601a0
yuzhichao <1932425337@qq.com>
2017-10-17 20:49:57 +0800

    wx

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -409,6 +409,24 @@ class ColumnController extends WebController
         }
 
 
+        // 参数不是自己的服务项目
+        $server = $this->PurposeModel->selectAll('use_server', ['use_id' => session('user_id', 1)]);
+        foreach ($server as $value) {
+            $unser = unserialize($value->server);
+            foreach ($unser as $v) {
+                foreach ($v as $va) {
+                    $serverAll[] = $va;
+                }
+            }
+        }
+
+        // 删除不再服务范围的服务
+
+        foreach ($need as $key => $value) {
+            if (!in_array($value->column_id, $serverAll)) {
+                $need[$key] = null;
+            }
+        }
         return view($this->file . 'waitbussiness')->with([
             'need' => $need,
             'id' => $id,
