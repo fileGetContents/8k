9b571bc710ddfef337b7f0573ca03d949a9e392d
yuzhichao <1932425337@qq.com>
2017-10-18 14:20:42 +0800

    'wx'

--- resources/views/Web/connectbussiness.blade.php
+++ resources/views/Web/connectbussiness.blade.php
@@ -39,35 +39,35 @@
         </span>
         </div>
     @endif
-
-    @foreach($need as $value)
-        <div class="section">
-            <a href="{{URL('demand/details2/'.$id[$num]->id2.'/'.$value->price)}}">
-                <div class="section-top">
-                    <button class="new">新</button>
-                    <span>({{ substr($value->telephone,0,3) }}***** {{substr($value->telephone,-3,3)}} )</span>
-                    <button class="connect">请尽快联系Ta</button>
-                </div>
-                <div class="section-mess">
-                    <p>正在寻找<span class="import">{{$value->column_name}}</span></p>
-                    <div class="clock"><img src="{{URL('img/clock.png')}}"></div>
-                    <div class="time">发布时间:{{ date('Y-m-d',$value->add_time) }}</div>
-                    <div class="mess-right">
-                        <div class="data">
-                            <div>{{$value->quote}}</div>
-                            @if( $value->quote ==0 )
-                                <div>当前无人抢单</div>
-                            @else
-                                <div>{{ $value->quote }}</div>
-                            @endif
-                        </div>
-                        <div class="ji">急</div>
+    @if(!empty($need))
+        @foreach($need as $value)
+            <div class="section">
+                <a href="{{URL('demand/details2/'.$id[$num]->id2.'/'.$value->price)}}">
+                    <div class="section-top">
+                        <button class="new">新</button>
+                        <span>({{ substr($value->telephone,0,3) }}***** {{substr($value->telephone,-3,3)}} )</span>
+                        <button class="connect">请尽快联系Ta</button>
                     </div>
-                </div>
-            </a>
-        </div>
-    @endforeach
-
+                    <div class="section-mess">
+                        <p>正在寻找<span class="import">{{$value->column_name}}</span></p>
+                        <div class="clock"><img src="{{URL('img/clock.png')}}"></div>
+                        <div class="time">发布时间:{{ date('Y-m-d',$value->add_time) }}</div>
+                        <div class="mess-right">
+                            <div class="data">
+                                <div>{{$value->quote}}</div>
+                                @if( $value->quote ==0 )
+                                    <div>当前无人抢单</div>
+                                @else
+                                    <div>{{ $value->quote }}</div>
+                                @endif
+                            </div>
+                            <div class="ji">急</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    @endif
     <div class="nofound">
         <p><img src="{{asset('img/nofound.png')}}"></p>
         <p>没有更多记录了</p>
