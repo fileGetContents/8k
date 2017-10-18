d8b8dc3540a9dfb607c862602f389173a732d783
yuzhichao <1932425337@qq.com>
2017-10-18 09:42:03 +0800

    'wx'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,8 @@ class UserController extends WebController
         $wx = new WechateController();
         $wx->wxUserLogin(URL('person'));
         $user = $this->PurposeModel->selectFirst('use', ['id' => session('user_id', 1)]);
+        dump($user);
+        die;
         return view($this->file . 'person')->with(
             ['user' => $user]
         );
