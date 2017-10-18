46ccf8fb04970e75f1fde6802eb8afd85c2ba875
yuzhichao <1932425337@qq.com>
2017-10-18 14:19:17 +0800

    'wx'

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -342,11 +342,8 @@ class ColumnController extends WebController
     public function connectbussiness(Request $request)
     {
         $need = self::selWhereAll(['server_id' => session('user_id', 2)]);
-
-        dump($need);
-die;
         return view($this->file . 'connectbussiness')->with([
-            'need' => self::selWhereAll(['server_id' => session('user', 2)]),
+            'need' => self::selWhereAll(['server_id' => session('user_id', 2)]),
             'id' => self::selFileAs('use_demand.id as id2', ['server_id' => session('user', 2)]),
             'num' => 0,
             'iden' => self::isIdentify()
